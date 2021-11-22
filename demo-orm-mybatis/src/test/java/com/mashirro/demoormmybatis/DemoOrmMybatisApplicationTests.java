a1557b43acc129bdee3c67dfc93038c9f82e6618
chenzg <18627901759@163.com>
2021-11-22 16:12:20 +0800

    编写junit5测试类

--- demo-orm-mybatis/src/test/java/com/mashirro/demoormmybatis/DemoOrmMybatisApplicationTests.java
+++ demo-orm-mybatis/src/test/java/com/mashirro/demoormmybatis/DemoOrmMybatisApplicationTests.java
@@ -1,13 +1,40 @@
 package com.mashirro.demoormmybatis;
 
+import com.mashirro.demoormmybatis.entity.User;
+import com.mashirro.demoormmybatis.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
+@Slf4j
 @SpringBootTest
 class DemoOrmMybatisApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private UserMapper userMapper;
+
+	/**
+	 * 测试查询所有
+	 */
+	@Test
+	public void selectAllUser() {
+		List<User> userList = userMapper.selectAllUser();
+		log.debug("【userList】= {}", userList);
+	}
+
+
+	/**
+	 * 测试根据主键删除
+	 */
+	@Test
+	public void deleteById() {
+		int i = userMapper.deleteById(1L);
 	}
 
 }
